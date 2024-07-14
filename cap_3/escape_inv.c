7c95f0b83739bbe3408ff69165679662b08b3787
Xipiii <m.calfio.c@uc.cl>
2024-07-14 13:42:50 -0400

    Termino de ejercicio seccion 3.4. Pagina 76

--- cap_3/escape_inv.c
+++ cap_3/escape_inv.c
@@ -5,22 +5,22 @@
 // copia la cadena t a s. Utilice un switch. Escriba también una función para la dirección
 // inversa, convirtiendo secuencias de escape en caracteres reales.
 
-// Debo copiar t a s y cada vez que haya un '\n', poner un '\\' y luego un 'n, y si es un '\t', un '\\' y un 't'.
+// Con un concepto similar al original, debo ir copiando uno a uno los caracteres. Si el caracter es \ y el siguiente es t, o n, entonces pongo su respectiva sec. de
+// escape
 
 #define MAX 100
 
-void escape(char s[], char t[], int lim){ 
-    int i, j; // Uno que revisa t y otro que escribe en s.
-
-    for (i = j = 0; j < lim - 1 && t[i] != '\0'; ++i) 
+void escape_inv(char s[], char t[], int lim){ 
+    int i, j; // i revisa, j escribe.
+    
+    for (i = j = 0; j < lim - 1 && t[i] != '\0'; ++i) // Un switch anidado necesita estar dentro de llaves?
         switch(t[i]) {
-            case '\n':
-                s[j++] = '\\';
-                s[j++] = 'n';
-                break;
-            case '\t':
-                s[j++] = '\\';
-                s[j++] = 't';
+            case '\\':
+                if (t[i + 1] == 'n')
+                    s[j++] = '\n';
+                else if (t[i + 1] == 't')
+                    s[j++] = '\t';
+                ++i; // Se ajusta el indice para que el proximo sea una letra nada que ver con \n y \t
                 break;
             default:
                 s[j++] = t[i];
@@ -28,7 +28,7 @@ void escape(char s[], char t[], int lim){
         }
     
     s[j++] = '\n';
-    s[j] = '\0'; 
+    s[j] = '\0';
 }
 
 void getline(char t[], int lim){
@@ -47,8 +47,8 @@ void main() {
 
     getline(palabra, MAX);
 
-    escape(modificada, palabra, MAX);
+    escape_inv(modificada, palabra, MAX);
     
     printf("Original: %sModificada: %s \n", palabra, modificada);
 }
-// Palabra de prueba: "palabra tab  tab   palabra"
+// Palabra de prueba: "palabra tab\ttab\tpalabra\n"
