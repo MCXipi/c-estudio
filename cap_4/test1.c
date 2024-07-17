7e672497530501feadc02c7f5f0b5fcfbf495b7f
Xipiii <m.calfio.c@uc.cl>
2024-07-17 19:58:10 -0400

    Inicio capitulo 4 testeo funciones separadas. Modificacion gitignore. Pagina 89.

---
+++ cap_4/test1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// Test para probar la funcionalidad de compilar un programa a partir de varios archivos fuente
+
+void main() {
+    imprimir_holamundo();
+}
---
+++ cap_4/test2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// Test para probar la funcionalidad de compilar un programa a partir de varios archivos fuente
+
+void imprimir_holamundo() {
+    printf("Hola mundo! Estoy separado.\n");
+}
