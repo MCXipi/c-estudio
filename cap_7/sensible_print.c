475f0e5f3fbeaf4d48493469cbae84b066a5af55
Xipiii <m.calfio.c@uc.cl>
2024-08-25 17:52:00 -0400

    Fin ejercicio 7-2. Pagina 181.

--- cap_7/sensible_print.c
+++ cap_7/sensible_print.c
@@ -2,31 +2,43 @@
 #include <ctype.h>
 
 #define MAXCOL 10
+#define TMPLEN 100
 
 int main () {
+    // Funcion que escribe la entrada recibida volviendo caracteres no graficos en graficos, y restringiendo las columnas a un numero MAXCOL
+    // Trabaja las palabras cortadas poniendo un "-" al continuarla en la otra fila
+    // Imprime codigo ASCII en forma hexadecimal de los caracteres no graficos.
+    
     int c, last, col;
-    char temp[MAXCOL];
+    char temp[TMPLEN];
 
     col = 1;
+    last = NULL;
 
     while((c = getchar()) != EOF) {
-        if (isprint(c) && col <= MAXCOL) {
+        if (isalpha(last) && isalpha(c) && col == 1) {
+            printf("-%c", c);
+            col += 2;
+        }
+        else if (isprint(c) && col < MAXCOL) {
             printf("%c", c);
             ++col;
         }
-        else if (iscntrl(c) && col <= MAXCOL) {
-            sprintf(temp, "\\0x%X", c);
-            printf("%s", temp);
-            col += strlen(temp);
-        }
-        else if (isgraph(c)) {
-            col = 1;
-            isgraph(last) ? printf("-\n-%c", c) : printf("\n%c", c);
-        }
         else if (iscntrl(c)) {
-            sprintf(temp, "\n0x%X", c);
-            printf("%s", temp);
-            col += strlen(temp);
+            sprintf(temp, "\\0x%X", c);
+            if (col + strlen(temp) > MAXCOL) {
+                col = 1;
+                printf("\n%s", temp);
+                col += strlen(temp);
+            }
+            else {
+                printf("%s", temp);
+                col += strlen(temp);
+            }
+        }
+        else if (col >= MAXCOL) {
+            printf("%c\n", c);
+            col = 1;
         }
         last = c;
     }
